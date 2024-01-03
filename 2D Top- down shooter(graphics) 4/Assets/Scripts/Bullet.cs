894d51742c4e13da8fb0f169b735c77864835ec5
SantJagoBro <wtfoxgames@gmail.com>
2024-01-03 13:19:17 +0200

    Тест

--- 2D Top- down shooter(graphics) 4/Assets/Scripts/Bullet.cs
+++ 2D Top- down shooter(graphics) 4/Assets/Scripts/Bullet.cs
@@ -19,7 +19,6 @@ public class Bullet : MonoBehaviour
 
     [SerializeField] Type type;
 
-    // Start is called before the first frame update
     void Start()
     {
         Invoke(nameof(Death), deathTime);
